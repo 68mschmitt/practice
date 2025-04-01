2021fd60f48e577caee5f565c1b76345222c1029
68mschmitt <68mschmitt@gmail.com>
2025-04-01 17:18:13 -0400

    Do not output dots

--- c/my-ls/my-ls.c
+++ c/my-ls/my-ls.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     int count = 0;
     if (d) {
         while ((dir = readdir(d)) != NULL ) {
-            if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
+            if (strncmp(".", dir->d_name, 1) == 0) {
                 continue;
             }
             if (count >= cols && count % cols == 0) {
